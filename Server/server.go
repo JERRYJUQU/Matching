665e1ebf0d0b10afa7a1798ced489e77de3a3391
JERRYJUQU <jerryjuqu@gmail.com>
2022-05-08 15:05:02 -0400

    Add Hello world

--- Server/server.go
+++ Server/server.go
@@ -1,5 +1,19 @@
-package matching
+package main
+
+import "net/http"
+
+type HTTPHandler struct{}
+
+const SERVER_ADDR = "127.0.0.1:8080"
+
+func (handler HTTPHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	data := []byte("Hello World!")
+
+	res.Write(data)
+}
 
 func main() {
+	var handler HTTPHandler
 
+	http.ListenAndServe(SERVER_ADDR, handler)
 }
